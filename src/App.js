eda9950a2cb312620518857d93e5167b6e69698e
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-10-29 20:32:54 +0100

    added Heero and Features components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <Features />
     </div>
   );
 };
